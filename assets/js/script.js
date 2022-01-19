c7e117b732efad2cb7f759a644014a744efeb230
NotANewt <meegafauna@gmail.com>
2022-01-18 18:28:10 -0600

    psudocoding with class in office hours

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// global variables
+
+// functions
+
+//event listeners
+
+//on page load, show any past cities searched from local storage
+
+//search for city button click
+
+//click on recent city searches to show weather
